73cc8abf0ff98c92b6e7ec7383ff295823a7105e
summerfences <89235551+summerfences@users.noreply.github.com>
2022-01-18 20:52:53 -0600

    Add files via upload

---
+++ python/python-aio/bk1ch4app.py
@@ -0,0 +1,11 @@
+# This is a Python comment in the book's first Python app.
+quantity = 14
+
+# This variable contains a float
+unit_price = 26.99
+
+# This variable contains the result of multiplying quantity times unit price
+extended_price = quantity * unit_price
+
+# Show the results
+print(extended_price)
