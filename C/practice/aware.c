19db61da4b0e15323f59bef865e54cc35688bd48
summerfences <89235551+summerfences@users.noreply.github.com>
2022-01-18 21:01:15 -0600

    Create aware.c

---
+++ C/practice/aware.c
@@ -0,0 +1,14 @@
+/* How do I meme? */
+#include<stdio.h>
+
+int main()
+{
+	printf("\n\n\n\n\n\nPress ENTER to ease my\npain in knowing that\nTHIS HARDWARE LACKS SENTIENCE.");
+	getchar();
+	printf("AG a ii NN");
+	getchar();
+	printf(".....again.....\n.....\n....\n...\n..\n.");
+	getchar();
+	printf("\n\nThx\n\n\n\n\n\n");
+	return 0;
+}
